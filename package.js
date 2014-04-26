d2e62af0496f02e276e6937bf63d7ce04e4dc371
Neil MacMunn <neil@gumstix.com>
2014-04-26 08:58:02 -0700

    Correctly include UI and export iframe.

--- package.js
+++ package.js
@@ -1,13 +1,14 @@
 Package.describe({
-  summary: "Detect when client is running in an iframe"
+    summary: "Detect when client is running in an iframe"
 });
 
 Package.on_use(function (api, where) {
-  api.add_files('iframe.js', ['client']);
+    api.use('ui');
+    api.add_files('iframe.js', ['client']);
+    api.export('iframe', ['client']);
 });
 
 Package.on_test(function (api) {
-  api.use('iframe');
-
-  api.add_files('iframe_tests.js', ['client']);
+    api.use('iframe');
+    api.add_files('iframe_tests.js', ['client']);
 });
